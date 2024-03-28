bb3f5c18638afd5343a061caf7aa79308a39a5e4
poojachede <154319147+poojachede@users.noreply.github.com>
2024-03-28 17:55:49 +0530

    Update main.tf

--- terraform-files/main.tf
+++ terraform-files/main.tf
@@ -1,24 +1,24 @@
-resource "aws_instance" "New-test-server" {
-  ami           = "ami-0c101f26f147fa7fd" 
+resource "aws_instance" "Test-Server" {
+  ami           = "ami-080e1f13689e07408" 
   instance_type = "t2.micro" 
-  key_name = "learnawskey"
-  vpc_security_group_ids= ["sg-020a949c964fb36e9"]
+  key_name = "poojakey"
+  vpc_security_group_ids= ["sg-0481160cc96026b15"]
   connection {
     type     = "ssh"
-    user     = "ec2-user"
-    private_key = file("./learnawskey.pem")
+    user     = "ubuntu"
+    private_key = file("./poojakey.pem")
     host     = self.public_ip
   }
   provisioner "remote-exec" {
     inline = [ "echo 'wait to start instance' "]
   }
   tags = {
-    Name = "New-test-server"
+    Name = "Test-Server"
   }
   provisioner "local-exec" {
-    command = "echo ${aws_instance.New-test-server.public_ip} > inventory"
+    command = "echo ${aws_instance.Test-Server.public_ip} > inventory"
   }
   provisioner "local-exec" {
-    command = "ansible-playbook /var/lib/jenkins/workspace/Banking-Project/terraform-files/ansibleplaybook.yml"
+    command = "ansible-playbook /var/lib/jenkins/workspace/Banking-Finance-Project/terraform-files/ansiblefile.yml"
   }
   }
